e7c54b89cd103d28994ba3e5a944fb2c8cf03e0d
Arya <arya.himant@gmail.com>
2018-12-20 13:47:23 +0530

    converted existing project into maven and implemented bubble sort

--- src/main/com/learn/himant/array/BubbleSort.java
+++ src/main/com/learn/himant/array/BubbleSort.java
@@ -1,5 +1,26 @@
 package com.learn.himant.array;
 
+/**
+ * Program to sort an given integer array using Bubble sort algorithm
+ * @author Himant Dewangan
+ *
+ */
 public class BubbleSort {
 
+	public int[] sort(int[] arr) {
+		for(int i = 0; i< arr.length; i++) {
+			for(int j = i+1; j < arr.length; j++) {
+				if(arr[i] > arr[j]) {
+					swap(arr, i, j);
+				}
+			}
+		}
+		return arr;
+	}
+
+	private void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 }
---
+++ src/test/com/learn/himant/array/BubbleSortTest.java
@@ -0,0 +1,27 @@
+package com.learn.himant.array;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BubbleSortTest {
+
+	@Test
+	public void shouldSortInAcendingOrder() {
+		BubbleSort bubbleSort = new BubbleSort();
+		int arr[] = {3, 2, 7, 5, 1, 0};
+		int sortedArr[] = {0, 1, 2, 3, 5, 7};
+		bubbleSort.sort(arr);
+		assertArrayEquals(sortedArr, arr);
+	}
+	
+	@Test
+	public void shouldSortRepeatingElementsToo() {
+		BubbleSort bubbleSort = new BubbleSort();
+		int arr[] = {3, 2, 7, 5, 1, 0, 7, 3};
+		int sortedArr[] = {0, 1, 2, 3, 3, 5, 7, 7};
+		bubbleSort.sort(arr);
+		assertArrayEquals(sortedArr, arr);
+	}
+	
+}
