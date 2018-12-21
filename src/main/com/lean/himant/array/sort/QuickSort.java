b4bdf3c093195242a08b4a5832b01dc714626743
Arya <arya.himant@gmail.com>
2018-12-21 12:30:26 +0530

    Quick sort explanation

--- src/main/com/lean/himant/array/sort/QuickSort.java
+++ src/main/com/lean/himant/array/sort/QuickSort.java
@@ -1,5 +1,24 @@
 package com.lean.himant.array.sort;
 
+/**
+ * First select the partition index in order to sort the array.
+ * For selecting partition index :
+ * 	. First select the pivot value (the last element in the array)
+ *  . Place the pivot in array such that, all the smaller values are to the left of the pivote and
+ *    greater elements are to the right side of the pivot.
+ *  . return the place of pivot
+ *  
+ * After selecting Partition, the pivot value is at correct place
+ * then repeat the process for two section of the array
+ * 
+ * Sort(array, low, p-1)  --> where p is the position of pivot
+ * sort(array, p+1, high)
+ * 
+ * The complexity is 0(n^2) in worst case scenario, best case O(n log(n))
+ * 
+ * @author Himant Dewangan
+ *
+ */
 public class QuickSort {
 
 	public void sort(final int[] arr, int low, int high) {
@@ -7,7 +26,6 @@ public class QuickSort {
 			int p = partition(arr, low, high);
 			sort(arr, low, p -1);
 			sort(arr, p +1, high);
-			
 		}
 	}
 
